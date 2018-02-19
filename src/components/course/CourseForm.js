983e9d6d1368105e4677c28262afa0fe5fe19146
webdude <dimo.petrof@gmail.com>
2018-02-19 21:58:04 +0200

    use destructuring more extensively

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -2,48 +2,52 @@ import React, { PropTypes } from 'react';
 import TextInput from "../common/TextInput";
 import SelectInput from "../common/SelectInput";
 
-const CourseForm = ({ course, allAuthors, onSave, onChange, loading, errors }) => (
-  <form>
-    <h1>Manage Course</h1>
-    <TextInput
-      name="title"
-      label="Title"
-      value={course.title}
-      onChange={onChange}
-      error={errors.title}
-    />
-    <SelectInput
-      name="authorId"
-      label="author"
-      value={course.authorId}
-      defaultOption="Select Author"
-      options={allAuthors}
-      onChange={onChange}
-      error={errors.authorId}
-    />
-    <TextInput
-      name="category"
-      label="Category"
-      value={course.category}
-      onChange={onChange}
-      error={errors.category}
-    />
-    <TextInput
-      name="length"
-      label="Length"
-      value={course.length}
-      onChange={onChange}
-      error={errors.length}
-    />
-    <input
-      type="submit"
-      disabled={loading}
-      value={loading ? "Saving..." : "Save"}
-      className="btn btn-primary"
-      onClick={onSave}
-    />
-  </form>
-);
+const CourseForm = ({ course, allAuthors, onSave, onChange, loading, errors }) => {
+  const { title, authorId, category, length } = course;
+
+  return (
+    <form>
+      <h1>Manage Course</h1>
+      <TextInput
+        name="title"
+        label="Title"
+        value={title}
+        onChange={onChange}
+        error={title}
+      />
+      <SelectInput
+        name="authorId"
+        label="author"
+        value={authorId}
+        defaultOption="Select Author"
+        options={allAuthors}
+        onChange={onChange}
+        error={authorId}
+      />
+      <TextInput
+        name="category"
+        label="Category"
+        value={category}
+        onChange={onChange}
+        error={category}
+      />
+      <TextInput
+        name="length"
+        label="Length"
+        value={length}
+        onChange={onChange}
+        error={length}
+      />
+      <input
+        type="submit"
+        disabled={loading}
+        value={loading ? "Saving..." : "Save"}
+        className="btn btn-primary"
+        onClick={onSave}
+      />
+    </form>
+  );
+};
 
 CourseForm.propTypes = {
   course: PropTypes.object.isRequired,
