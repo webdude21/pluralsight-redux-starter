605ffac8128465343290e02de2178d8a602df76e
webdude <dimo.petrof@gmail.com>
2018-02-24 16:42:56 +0200

    test course reducer

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -4,7 +4,11 @@ import * as actions from '../actions/courseActions';
 
 describe('Course Reducer', () => {
   it('should add course when passed CREATE_COURSE_SUCCESS', () => {
-    const initialState = [{ id: 1, title: 'Web development' }, { id: 2, title: 'Databases' }];
+    const initialState = [
+      { id: 1, title: 'Web development' },
+      { id: 2, title: 'Databases' }
+    ];
+
     const newCourse = { title: 'Data Structures' };
 
     const actual = courseReducer(initialState, actions.updateCourseSuccess(newCourse));
@@ -14,5 +18,52 @@ describe('Course Reducer', () => {
     expect(actual[0].title).toEqual('Web development');
     expect(actual[1].title).toEqual('Databases');
     expect(actual[2].title).toEqual('Data Structures');
+  });
+
+  it('should delete course when passed DELETE_COURSE', () => {
+    const initialState = [
+      { id: 1, title: 'Web development' },
+      { id: 2, title: 'Databases' },
+      { id: 3, title: 'Data Structures' }
+    ];
+
+    const actual = courseReducer(initialState, actions.deleteCourse(initialState[1]));
+
+    expect(actual.length).toBe(2);
+
+    expect(actual[0].title).toEqual('Web development');
+    expect(actual[1].title).toEqual('Data Structures');
+  });
+
+  it('should sort the courses when passed SORT_COURSE', () => {
+    const initialState = [
+      { id: 1, title: 'Web development' },
+      { id: 2, title: 'Databases' },
+      { id: 3, title: 'Data Structures' }
+    ];
+
+    const actual = courseReducer(initialState, actions.sortCourses('title'));
+
+    expect(actual.length).toBe(3);
+
+    expect(actual[0].title).toEqual('Data Structures');
+    expect(actual[1].title).toEqual('Databases');
+    expect(actual[2].title).toEqual('Web development');
+  });
+
+  it('should update the course when passed UPDATE_COURSE_SUCCESS', () => {
+    const initialState = [
+      { id: 1, title: 'Web development' },
+      { id: 2, title: 'Databases' },
+      { id: 3, title: 'Data Structures' }
+    ];
+
+    const actual = courseReducer(initialState, actions.updateCourseSuccess({ id: 3, title: 'Data Structures 2' }));
+
+    expect(actual.length).toBe(3);
+
+    expect(actual[0].title).toEqual('Web development');
+    expect(actual[1].title).toEqual('Databases');
+    expect(actual[2].title).toEqual('Data Structures 2');
   });
 });
