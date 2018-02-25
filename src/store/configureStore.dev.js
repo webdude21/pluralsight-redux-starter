05a3dc45c4f0d09369289c0ebb838a9c1f28b7c8
webdude <dimo.petrof@gmail.com>
2018-02-25 17:53:02 +0200

    add redux-devtools-extension

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -4,11 +4,12 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import loggerMiddleware from '../middleware/LoggingMiddleware';
 import errorMiddleware from '../middleware/ErrorHandlingMiddleware';
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, loggerMiddleware, errorMiddleware, reduxImmutableStateInvariant())
+    composeWithDevTools(applyMiddleware(thunk, loggerMiddleware, errorMiddleware, reduxImmutableStateInvariant()))
   );
 }
